3409e9d871c95740fe8d3df6064e7259a1cc885b
teamzhang <teamzhang@Antelope.local>
2017-05-22 10:25:49 +0800

    add disterbuted

--- textgitlab/textgitlab/AppDelegate.m
+++ textgitlab/textgitlab/AppDelegate.m
@@ -16,7 +16,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    UIWindow *window = [[UIWindow alloc] initWithFrame:CGRectMake(0, 0, 890, 900)];
+    
     return YES;
 }
 
